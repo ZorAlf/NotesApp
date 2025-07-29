e6a0ffaf5231db8e0cdc08ce5e13d2c5fc1b912c
ZorAlf <Alfandia860@gmail.com>
2025-07-29 11:37:28 +0700

    add some logic to views.py

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
+from .forms import NoteForm
 
 # Create your views here.
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/notes_list.html', {'notes': notes})
+
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/add_note.html', {'form': form})
+
+def note_edit(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/edit_note.html', {'form': form, 'note': note})
+
+def note_delete(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        note.delete()
+        return redirect('note_list')
+    return render(request, 'notes/delete_note.html', {'note': note})
